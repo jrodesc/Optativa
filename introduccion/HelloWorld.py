18d9e177a2d4d8819094442bbd8ec61154f768a1
Jose Luis <jrodesc1511@gmail.com>
2025-09-22 09:01:30 +0200

    primeros pasos en Python

--- introduccion/HelloWorld.py
+++ introduccion/HelloWorld.py
@@ -1,1 +1,34 @@
-print("Hello World")
+print("""
+Hello World
+Hola Mundo
+Bonjour le monde""")
+#hola mundo
+
+
+
+nombre_usuario = "Jose"
+
+saldo_cuenta = "150.20"
+
+edad = 24
+
+nombre = "Pepe"
+
+salario = 1800.40
+
+numero_complejo = -100 + 3.2j
+
+print(type(numero_complejo))
+
+
+#En python todos los valores son objetos
+#ya que todos heredan de la clase objeto
+#no existen los tipos primitivos, son los tipos basicos
+
+# Declaración de variables
+numero_entero = 10
+numero_real = 3.14
+numero_complejo = -1.23 + 4.5j
+cadena = "Hola"
+es_par = True
+
