962c7ebe8d45996feac276dcdb22d3e147af236e
Jose Luis <jrodesc1511@gmail.com>
2025-09-26 09:23:57 +0200

    ejemplo basico de calculadora

--- introduccion/ejemplos.py
+++ introduccion/ejemplos.py
@@ -1,3 +1,61 @@
-numero = int(input("introduce un numero: "))
+def suma(a, b):
+    return a + b
 
-print(numero)
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a // b
+
+def datosSumar():
+    firstVal = int(input("Escoja un numero: "))
+    secondVal = int(input("Escoja un numero: "))
+    resultado = suma(firstVal, secondVal)
+    return resultado
+
+def datosRestar():
+    firstVal = int(input("Escoja un numero: "))
+    secondVal = int(input("Escoja un numero: "))
+    resultado = resta(firstVal, secondVal)
+    return resultado
+
+def datosMultiplicar():
+    firstVal = int(input("Escoja un numero: "))
+    secondVal = int(input("Escoja un numero: "))
+    resultado = multiplicacion(firstVal, secondVal)
+    return resultado
+
+def datosDividir():
+    firstVal = int(input("Escoja un numero: "))
+    secondVal = int(input("Escoja un numero: "))
+    resultado = division(firstVal, secondVal)
+    return resultado
+#-----------------------------------------------------#
+
+
+
+
+
+while True:
+    print("1. para Sumar")
+    print("2. para Restar")
+    print("3. para Multiplicar")
+    print("4. para Dividir")
+    print("5. para Salir")
+    calculo = int(input("Escoja que operacion desea realizar: "))
+    match calculo:
+        case 1:
+            print(datosSumar())
+        case 2:
+            print(datosRestar())
+        case 3:
+            print(datosMultiplicar())
+        case 4:
+            print(datosDividir())
+        case 5:
+            break
+        case _:
+            print("Numero introducido no valido")
